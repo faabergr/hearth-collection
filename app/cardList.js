763a4f7ff20ca44a5b03718ec281c68223f3c1e1
Rick Faaberg <rick@faaberg.org>
2014-10-22 08:51:40 -0700

    Initial commit

---
+++ app/cardList.js
@@ -0,0 +1,32 @@
+/**
+ * Created by faabergr on 10/21/2014.
+ */
+(function() {
+    "use strict";
+
+    var deckManagerApp = angular.module('deckManagerApp', []);
+
+    deckManagerApp.controller('cardListCtrl', ['$scope', '$http', function($scope, $http) {
+        $http.get('data/all-cards.json')
+            .success(function(data, status, headers, config ) {
+                $scope.cards = data.cards;
+            })
+            .error(function(data, status, headers, config) {
+                console.log('error');
+            });
+    }]);
+
+    deckManagerApp.filter('filterCards', function() {
+       return function(arr, filterText) {
+           var outArr = [];
+           var filterText = filterText || "";
+           angular.forEach(arr, function(item) {
+               if (item.name.toLowerCase().indexOf(filterText.toLowerCase()) > -1)
+               {
+                   this.push(item);
+               }
+           }, outArr);
+           return outArr;
+       };
+    });
+}());
